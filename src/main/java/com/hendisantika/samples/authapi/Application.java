808fb89db9d847ba19f98fbc4f6a403391b35229
Hendi Santika <hendisantika@gmail.com>
2017-10-13 07:32:27 +0700

    #2 - Added Main Apps

---
+++ src/main/java/com/hendisantika/samples/authapi/Application.java
@@ -0,0 +1,22 @@
+package com.hendisantika.samples.authapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-auth
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/10/17
+ * Time: 05.42
+ * To change this template use File | Settings | File Templates.
+ */
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
