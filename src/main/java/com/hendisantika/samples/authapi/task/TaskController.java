4c05d1daa2c3c728584101c4e5af54fce132010e
Hendi Santika <hendisantika@gmail.com>
2017-10-13 07:33:57 +0700

    #5 - Added Task Controller

---
+++ src/main/java/com/hendisantika/samples/authapi/task/TaskController.java
@@ -0,0 +1,56 @@
+package com.hendisantika.samples.authapi.task;
+
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-auth
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/10/17
+ * Time: 05.48
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping("/")
+public class TaskController {
+
+	private TaskRepository taskRepository;
+
+	public TaskController(TaskRepository taskRepository) {
+		this.taskRepository = taskRepository;
+	}
+
+	@PostMapping
+	public void addTask(@RequestBody Task task) {
+		taskRepository.save(task);
+	}
+
+	@GetMapping
+	public List<Task> getTasks() {
+		return taskRepository.findAll();
+	}
+
+	@PutMapping("/{id}")
+	public void editTask(@PathVariable long id, @RequestBody Task task) {
+		Task existingTask = taskRepository.findOne(id);
+		Assert.notNull(existingTask, "Task not found");
+		existingTask.setDescription(task.getDescription());
+		taskRepository.save(existingTask);
+	}
+
+	@DeleteMapping("/{id}")
+	public void deleteTask(@PathVariable long id) {
+		taskRepository.delete(id);
+	}
+}
