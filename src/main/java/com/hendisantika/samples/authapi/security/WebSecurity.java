b07a325ac082ef0dfdd4afc5c0e32ddecd22cda3
Hendi Santika <hendisantika@gmail.com>
2017-10-22 12:08:36 +0700

    #15 - Added Web Security

--- src/main/java/com/hendisantika/samples/authapi/security/WebSecurity.java
+++ src/main/java/com/hendisantika/samples/authapi/security/WebSecurity.java
@@ -1,5 +1,15 @@
 package com.hendisantika.samples.authapi.security;
 
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static com.hendisantika.samples.authapi.security.SecurityConstants.SIGN_UP_URL;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : spring-boot-auth
@@ -10,5 +20,44 @@ package com.hendisantika.samples.authapi.security;
  * Time: 05.39
  * To change this template use File | Settings | File Templates.
  */
-public class WebSecurity {
+
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+	private UserDetailsService userDetailsService;
+
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	public WebSecurity(UserDetailsService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+
+		this.userDetailsService = userDetailsService;
+
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+
+	}
+
+	@Override
+
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.csrf().disable().authorizeRequests()
+
+				.antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
+
+				.anyRequest().authenticated()
+
+				.and()
+
+				.addFilter(new JWTAuthenticationFilter(authenticationManager()))
+
+				.addFilter(new JWTAuthorizationFilter(authenticationManager()));
+
+	}
+
+	@Override
+
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+
+	}
 }
