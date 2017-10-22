d43039432b12687e5a73228fd485a363ee56cd9c
Hendi Santika <hendisantika@gmail.com>
2017-10-22 12:09:07 +0700

    #16 - Added Application User

--- src/main/java/com/hendisantika/samples/authapi/user/ApplicationUser.java
+++ src/main/java/com/hendisantika/samples/authapi/user/ApplicationUser.java
@@ -1,5 +1,10 @@
 package com.hendisantika.samples.authapi.user;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : spring-boot-auth
@@ -10,5 +15,45 @@ package com.hendisantika.samples.authapi.user;
  * Time: 05.29
  * To change this template use File | Settings | File Templates.
  */
+
+@Entity
 public class ApplicationUser {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+
+	private long id;
+
+	private String username;
+
+	private String password;
+
+	public long getId() {
+
+		return id;
+
+	}
+
+	public String getUsername() {
+
+		return username;
+
+	}
+
+	public void setUsername(String username) {
+
+		this.username = username;
+
+	}
+
+	public String getPassword() {
+
+		return password;
+
+	}
+
+	public void setPassword(String password) {
+
+		this.password = password;
+
+	}
 }
