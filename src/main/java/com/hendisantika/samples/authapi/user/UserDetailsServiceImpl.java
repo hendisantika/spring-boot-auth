e7929d9a54cd06fdd4f5ba503d19d229b5353e78
Hendi Santika <hendisantika@gmail.com>
2017-10-23 06:11:55 +0700

    #20 - Added User Details Service Impl

--- src/main/java/com/hendisantika/samples/authapi/user/UserDetailsServiceImpl.java
+++ src/main/java/com/hendisantika/samples/authapi/user/UserDetailsServiceImpl.java
@@ -1,5 +1,13 @@
 package com.hendisantika.samples.authapi.user;
 
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import static java.util.Collections.emptyList;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : spring-boot-auth
@@ -10,5 +18,30 @@ package com.hendisantika.samples.authapi.user;
  * Time: 05.40
  * To change this template use File | Settings | File Templates.
  */
-public class UserDetailsServiceImpl {
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+	private ApplicationUserRepository applicationUserRepository;
+
+	public UserDetailsServiceImpl(ApplicationUserRepository applicationUserRepository) {
+
+		this.applicationUserRepository = applicationUserRepository;
+
+	}
+
+	@Override
+
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		ApplicationUser applicationUser = applicationUserRepository.findByUsername(username);
+
+		if (applicationUser == null) {
+
+			throw new UsernameNotFoundException(username);
+
+		}
+
+		return new User(applicationUser.getUsername(), applicationUser.getPassword(), emptyList());
+
+	}
 }
